6ce59f429a7872e2f74ad599405898902c4f7c1b
Subhasish Biswasray <subhasish9583672357@gmail.com>
2023-05-02 17:47:24 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './screens/About'
 export default function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
